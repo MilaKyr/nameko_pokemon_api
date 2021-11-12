a6ba8bc746d9ce2d72053ecfb159f5d4f88c4c68
MilaKyr <11293949+MilaKyr@users.noreply.github.com>
2021-11-12 22:07:09 +0100

    Add test for check_input

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,11 @@
 import pytest
 
-from app.utils import check_if_int, check_if_float, check_range
+from app.utils import check_input, check_if_float, check_if_int
 from app.errors import ValueTooSmall, ValueTooBig, InvalidValueType
 
 
-def test_check_if_float_with_str():
-    pytest.raises(InvalidValueType, check_if_float, "a")
+def test_check_input_with_str():
+    pytest.raises(InvalidValueType, check_input, "a")
 
 
 def test_check_if_float_with_float():
@@ -13,8 +13,8 @@ def test_check_if_float_with_float():
     assert result == 1.5
 
 
-def test_check_if_int_with_float():
-    pytest.raises(InvalidValueType, check_if_int, 1.5)
+def test_check_input_with_float():
+    pytest.raises(InvalidValueType, check_input, 1.5)
 
 
 def test_check_if_int_with_round_float():
@@ -23,12 +23,17 @@ def test_check_if_int_with_round_float():
 
 
 def test_range_limit_int_type_fail_with_zero():
-    pytest.raises(ValueTooSmall, check_range, 0)
+    pytest.raises(ValueTooSmall, check_input, 0)
 
 
 def test_range_limit_int_type_fail_with_negative():
-    pytest.raises(ValueTooSmall, check_range, -10)
+    pytest.raises(ValueTooSmall, check_input, -10)
 
 
 def test_range_limit_int_type_fail_with_too_high_id():
-    pytest.raises(ValueTooBig, check_range, 10_000)
+    pytest.raises(ValueTooBig, check_input, 10_000)
+
+
+def test_check_input():
+    result = check_input(10)
+    assert result == 10
